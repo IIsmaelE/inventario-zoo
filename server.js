070fdea09ec01ca83ce5016f3253f706636d1317
IIsmaelE <iep24413070160478@cecyteh.edu.mx>
2026-06-17 17:53:22 -0600

    Preparado para el despliegue

---
+++ server.js
@@ -0,0 +1,38 @@
+const express = require('express');
+const mongoose = require('mongoose');
+const cors = require('cors');
+require('dotenv').config();
+
+const app = express();
+app.use(express.json());
+app.use(cors());
+
+// Conexión a MongoDB Atlas
+mongoose.connect(process.env.MONGO_URI)
+.then(() => console.log(" Conexión exitosa a MongoDB Atlas"))
+.catch(err => console.error(" Error de conexión:", err));
+
+// Esquema NoSQL de Mongoose (Documento JSON)
+const ProductoSchema = new mongoose.Schema({
+nombre: String,
+precio: Number,
+existencia: Number
+});
+const Producto = mongoose.model('Producto', ProductoSchema);
+
+// RUTAS DE LA API
+// 1. GET: Obtener todos los productos
+app.get('/productos', async (req, res) => {
+const productos = await Producto.find();
+res.json(productos);
+});
+
+// 2. POST: Insertar un nuevo producto
+app.post('/productos', async (req, res) => {
+const nuevoProducto = new Producto(req.body);
+await nuevoProducto.save();
+res.json({ mensaje: "Producto registrado", nuevoProducto });
+});
+
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => console.log(`Servidor activo en puerto ${PORT}`));
